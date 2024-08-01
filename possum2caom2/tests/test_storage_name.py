1100ac7e4d15298c3271327fca635ab988d84418
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2024-07-31 17:53:32 -0700

    CADC-13609 - fpack'd files have an additional HDU.

--- possum2caom2/tests/test_storage_name.py
+++ possum2caom2/tests/test_storage_name.py
@@ -86,6 +86,7 @@ def test_storage_name(test_config, test_name):
         '1367MHz_18asec_2039-5115_10973_pilot1_v1',
         '1368MHz_18asec_2013-5552_11261_pilot1_v2',
         '944MHz_18asec_2013-5553_11261_v1',
+        '943MHz_20asec_1321-4634_10612_v1',
     ]
     test_f_name = basename(test_name)
     test_uri = f'{test_config.scheme}:{test_config.collection}/{test_f_name.replace(".header", "")}'

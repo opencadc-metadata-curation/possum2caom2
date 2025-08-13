c00cb571f5e12d78a5e7d063e0f5eefc441e40d0
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2025-08-13 14:02:57 -0700

    CADC-14562 - API regression backwards-compatibility. (#19)
    * POSSUM - bring it up to caom2pipe main API.
    
    * CADC-13533 - fix git issues from changing machines.
    
    * CADC-13533 - merge clean up.
    
    * CADC-14562 - regression API matching.

--- possum2caom2/fits2caom2_augmentation.py
+++ possum2caom2/fits2caom2_augmentation.py
@@ -1,7 +1,7 @@
 # ******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 # *************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 #
-#  (c) 2021.                            (c) 2021.
+#  (c) 2025.                            (c) 2025.
 #  Government of Canada                 Gouvernement du Canada
 #  National Research Council            Conseil national de recherches
 #  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -74,7 +74,7 @@ from possum2caom2.main_app import mapping_factory
 __all__ = ['PossumFits2caom2Visitor']
 
 
-class PossumFits2caom2Visitor(cc.Fits2caom2Visitor):
+class PossumFits2caom2Visitor(cc.Fits2caom2Visitor2):
     def __init__(self, observation, **kwargs):
         super().__init__(observation, **kwargs)
 
